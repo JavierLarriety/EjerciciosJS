ecdd0e81aa9957f39dbc87538c73afa06021dd08
Javier Larriety Infanzón <javier@debian-BULLSEYE-live-builder-AMD64>
2022-02-14 23:11:24 +0100

    realizado ejercicio1

---
+++ 1-variables/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const nombre = "Javier";
+let color = "rojo";
+let edad = "33";
+
+console.log("Hola me llamo "+nombre, " tengo" +edad +" años y mi color favorito es el " +color);
+console.log(`Hola, me llamo ${nombre},tengo ${edad} años y mi color favorito es el ${color}`);
+
+
